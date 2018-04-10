21c4dcdbf7873facb0b378b297783acf7072805e
hamza <hamza.3738@gmail.com>
2018-04-10 20:07:06 +0500

    config changes

---
+++ LoaderApiAndAdmin/MappingInitializer.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactManagementSystem26_10_17
+{
+    public static class MappingInitializer
+    {
+        public static void InitializeMapping()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                //cfg.CreateMap<User, UserDto>();
+                //cfg.CreateMap<UserDto, User>();
+
+                //cfg.CreateMap<Item, ItemDto>();
+                //cfg.CreateMap<ItemDto, Item>();
+            });
+        }
+    }
+}
