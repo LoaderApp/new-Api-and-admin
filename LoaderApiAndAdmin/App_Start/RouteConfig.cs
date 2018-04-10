3b5017af1533a75cbd64d51e8a018315e2d374e4
hamza <hamza.3738@gmail.com>
2018-04-10 19:58:43 +0500

    Cors Changes

--- LoaderApiAndAdmin/App_Start/RouteConfig.cs
+++ LoaderApiAndAdmin/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace LoaderApiAndAdmin
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Admin", action = "AdminMain", id = UrlParameter.Optional }
             );
         }
     }
